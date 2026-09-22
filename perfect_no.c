57b146b261134a6eae4828a92d5a40a2b019bdd3
shreeshivani24-ai <shreeshivani24@gmail.com>
2026-09-22 20:27:30 +0530

    Add files via upload

---
+++ perfect_no.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n/2; i++)
+    {
+        if (n % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    if (sum == n)
+    {
+        printf("It is a perfect number.", n);
+    }
+    else
+    {
+        printf("It is not a perfect number.", n);
+    }
+    return 0;
+}
